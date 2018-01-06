1af1016346fa1903baad59355d9dd9d93e4ce99a
Ibiyemi Adewakun <ibywaks@gmail.com>
2018-01-06 22:43:48 +0100

    first routes created

---
+++ controllers.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+// HomeController - renders the home route
+func HomeController() {
+	fmt.Println("Welcome!")
+}
--- index.go
+++ index.go
@@ -1,7 +1,15 @@
 package main
 
-// import "github.com/kataras/iris"
+import (
+	"github.com/kataras/iris"
+)
 
 func main() {
-	// app := iris.New()
+	app := iris.New()
+
+	app.Get("/", func(ctx iris.Context) {
+		HomeController()
+	})
+
+	app.Run(iris.Addr("127.0.0.1:1234"))
 }
